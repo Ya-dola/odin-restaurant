4968a6bf1efa76afa6a3424fb49cebfb51f2e7ec
Yadola <Ya-dola@users.noreply.github.com>
2023-06-13 21:43:39 +0530

    Added Webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function contentDiv() {
+    const divContent = document.createElement('div');
+    divContent.id = 'content';
+
+    return divContent;
+}
+
+document.body.appendChild(contentDiv());
